093e591c73a69dd9dea754cd5cf7d7b8b5447992
duydc <dinhcongduy125@gmail.com>
2020-06-19 14:56:44 +0700

    config vi

--- lib/date_symbol_data_local.dart
+++ lib/date_symbol_data_local.dart
@@ -16669,21 +16669,21 @@ Map<dynamic, dynamic> dateTimeSymbolMap() => {
           ],
           SHORTWEEKDAYS: const [
             'CN',
-            'Th 2',
-            'Th 3',
-            'Th 4',
-            'Th 5',
-            'Th 6',
-            'Th 7'
+            'T2',
+            'T3',
+            'T4',
+            'T5',
+            'T6',
+            'T7'
           ],
           STANDALONESHORTWEEKDAYS: const [
             'CN',
-            'Th 2',
-            'Th 3',
-            'Th 4',
-            'Th 5',
-            'Th 6',
-            'Th 7'
+            'T2',
+            'T3',
+            'T4',
+            'T5',
+            'T6',
+            'T7'
           ],
           NARROWWEEKDAYS: const ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
           STANDALONENARROWWEEKDAYS: const [
